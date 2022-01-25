cc6080b8c330f1bd259038868bf00fa60d931ff2
Sweet-salt <79562182+Sweet-salt@users.noreply.github.com>
2022-01-25 22:29:26 +0900

    Update 10818.js
    2가지 방법으로 풀어보기

--- 10818.js
+++ 10818.js
@@ -5,12 +5,18 @@ let n = Number(input[0]);
 
 let data = input[1].split(' ').map(x => Number(x));
 
-let minValue = 1000000;
-let maxValue = -1000000;
+// let minValue = 1000000;
+// let maxValue = -1000000;
 
-for (let i = 0; i < n; i++){
-  let curValue = data[i]
-  if(curValue < minValue) minValue = curValue;
-  if(curValue > maxValue) maxValue = curValue;
-}
+// for (let i = 0; i < n; i++){
+//   let curValue = data[i]
+//   if(curValue < minValue) minValue = curValue;
+//   if(curValue > maxValue) maxValue = curValue;
+// }
+// console.log(minValue + " " + maxValue);
+
+// 2번째 방법 reducer사용해서 풀기
+let minValue = data.reduce((a,b) => Math.min(a, b));
+let maxValue = data.reduce((a,b) => Math.max(a, b));
+
 console.log(minValue + " " + maxValue);
